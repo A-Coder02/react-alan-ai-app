795fdc880c49e0bfea2704ea4dcf0f9e1ffa6272
Arbaj Ansari <arbaj.ansari@fladdra.com>
2022-12-25 21:14:40 +0530

    failed due to alan npm version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 const alanKey =
   "6557969cd82e6992fdf5d3f55c7e2ebb2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const [articles, setArticles] = useState({});
+
+  console.log({
+    articles,
+  });
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command }) => {
+      onCommand: ({ command, d }) => {
         if (command === "testCommand") {
           alert("this code was executed");
+        }
+        if (command === "newHeadlines") {
+          alert("tada");
+          setArticles(articles);
         }
       },
     });

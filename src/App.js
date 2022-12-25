39386d17565a446f3c151fd3ed47b0ddd8434164
Arbaj Ansari <arbaj.ansari@fladdra.com>
2022-12-25 16:22:35 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "6557969cd82e6992fdf5d3f55c7e2ebb2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("this code was executed");
+        }
+      },
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Alan AI React App</h1>
     </div>
   );
 }
